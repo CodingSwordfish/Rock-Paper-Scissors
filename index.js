aa9e4c3f4e08b91c1449927ed5e384baea33cf8c
CodingSwordfish <wagobryan@gmail.com>
2024-07-03 10:59:41 +0300

    Create an if/else statement chain Compare the randomNumber value to the numbers between 0 and 3 and log the string rock, paper or scissor depending on its value as constrained by the operators

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function getComputerChoice() {
    let randomNumber = Math.random() * 3; 
    console.log(randomNumber);
+      if(randomNumber<1){
+    console.log("Rock")
+   } else if (randomNumber>=1 && randomNumber<2){
+    console.log("Paper")
+   } else {
+    console.log("Scissors")
+   }
 }
+getComputerChoice()
+
